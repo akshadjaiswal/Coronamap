7934aaf64de90a47938106148f67228b9079bad7
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2022-10-11 16:51:37 +0530

    update

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ function updateMap()
  fetch("/data.json")
  .then(response => response.json())
  .then(rsp=>{
-    console.log(rsp)
+    console.log(rsp.data)
+    res.data.forEach(element => {
+        latitude = element.latitude;
+        longitude = element.longitude;
+
+        //Mark on the map
+    });
  })
 }
 updateMap(); 
